ca5c009054081c3841161bb7a86da7c13b29a39e
savior131 <91454389+savior131@users.noreply.github.com>
2023-09-01 16:07:02 +0300

    savior
    fixed what gihub has ruined

--- Assets/Scripts/Player/Health/PlayerHealth.cs
+++ Assets/Scripts/Player/Health/PlayerHealth.cs
@@ -1,3 +1,4 @@
+using SpriteGlow;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,22 +7,48 @@ public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int health;
     [SerializeField] ParticleSystem explotion;
+    [SerializeField] SpriteGlowEffect playerSprite;
     public event Action onHealthChange;
+    bool canDamegePlayer = true;
 
     public void decreaseHealth()
     {
-        health--;
-        if (onHealthChange != null) onHealthChange();
-        if (health < 1)
+        if (canDamegePlayer)
         {
-            explotion.transform.position = transform.position;
-            explotion.Play();
-            Destroy(gameObject);
+            health--;
+            if (onHealthChange != null) onHealthChange();
+            if (health < 1)
+            {
+                explotion.transform.position = transform.position;
+                explotion.Play();
+                Destroy(gameObject);
+            }
+            StartCoroutine(coolDown());
         }
     }
 
     public int getPlayerHealth()
     {
         return health;
+    }
+    IEnumerator coolDown()
+    {
+        canDamegePlayer = false;
+        StartCoroutine(coolDownEffect());
+        yield return new WaitForSeconds(1.5f);
+        StopAllCoroutines();
+        playerSprite.AlphaThreshold = 1;
+        canDamegePlayer = true;
+    }
+
+    IEnumerator coolDownEffect()
+    {
+        while (true)
+        {
+            playerSprite.AlphaThreshold = 0;
+            yield return new WaitForSeconds(0.1f);
+            playerSprite.AlphaThreshold = 1;
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
